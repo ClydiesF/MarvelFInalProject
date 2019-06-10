92c8086db4733227f25042229dea5b6a4c10d676
ABeck617 <abeckford532@gmail.com>
2019-06-10 10:23:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const app = express();
 
-const port = 3000;
+// PORT
+const port = process.env.PORT || 3000;
 
+//This is how you define a route
 app.get('/', (req, res) => {
-    res.render('reservation.ejs');
+    res.render('index.ejs');
   });
 
-app.post('/', (req, res) => { 
-    myNotes.push(req.body.note);
-    res.redirect('/');
-  });
+// app.post('/', (req, res) => { 
+//     myNotes.push(req.body.note);
+//     res.redirect('/');
+//   });
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`))
